5d41aa28e76459b2630a774c59a16c8c080aa795
ThomasAvuni <eclissifasica@gmail.com>
2025-05-18 22:51:47 +0200

    Commendi pt1

--- src/App.java
+++ src/App.java
@@ -6,23 +6,22 @@ import javafx.stage.StageStyle;
 import javafx.fxml.FXMLLoader;
 
 public class App extends Application {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception { 
         launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml"));
-        Scene scene = new Scene(loader.load(), 1024, 768);
+    public void start(Stage stage) throws Exception { // Metodo principale JavaFX
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("Main.fxml")); // Carica il file FXML
+        Scene scene = new Scene(loader.load(), 1024, 768); // Crea la scena
         stage.setTitle("Ristorante");
-        scene.setFill(Color.TRANSPARENT);
+        scene.setFill(Color.TRANSPARENT);   //Finestra trasparente e senza bordo
         stage.initStyle(StageStyle.TRANSPARENT);
         stage.setScene(scene);
         stage.setResizable(false);
-        ((MainController)loader.getController()).init(stage);
-        stage.show();
+        ((MainController)loader.getController()).init(stage); // Inizializza il controller
+        stage.show(); //Mosta la finestra
         Azienda a = new Azienda();
-        a.inizia();
+        a.inizia();// Inizializza l'azienda
     }
-
 }
--- src/Cameriere.java
+++ src/Cameriere.java
@@ -3,28 +3,34 @@ import java.util.Vector;
 import javafx.application.Platform;
 
 public class Cameriere extends Dipendente {
+    //Vettore di piatti pronti
     Vector<Piatto> piattiPronti;
+    //Costruttore
     public Cameriere(String nome, String cognome, Data dataNascita) {
         super(nome, cognome, dataNascita);
         piattiPronti = new Vector<Piatto>();
         this.ruolo = "Cameriere";
     }
 
+    //Funzione che setta lo stipendio del cameriere
     @Override
     public void calcolaStipendio() {
         setStipendio(1500);
     }
 
+    //Funzione che aggiunge un cibo ordinato alla coda di preparazione
     public void prendiOrdine(Cliente cliente, Cibo cibo) {
         if(cibo != null){
             Azienda.getChef().AddCiboInCoda(cibo);
         }
     }
 
+    //Funzione che aggiunge un piatto al vettore di piatti pronti
     public void AddPiattoPronto(Piatto piatto) {
         piattiPronti.addElement(piatto);
     }
 
+    //Funzione che controlla se il piatto ordinato è nel menu
     public boolean ControllaOrdinazione(String ordinazione){
         if(ordinazione.equalsIgnoreCase("pasta e zucchine") || ordinazione.equalsIgnoreCase("pasta al pomodoro") || ordinazione.equalsIgnoreCase("carne alla griglia")){
             return false;
@@ -34,23 +40,32 @@ public class Cameriere extends Dipendente {
     }
 
     public void portaPiatti() {
+        //Creazione di un thread anonimo per la consegna dei piatti
         new Thread(){
             public void run() {
+                //Iterazione sui piatti pronti
                 for (int i = 0; i < piattiPronti.size(); i++){
+                    //Variabile di supporto per il piato
                     Piatto piatto = piattiPronti.elementAt(i);
                     try {
+                        //Fermo il thread per il tempo di preparazione del piatto
                         Thread.sleep(piatto.getTempoDiPreparazione() * 1000);
+                        //Mostro l'interfaccia grafica per la consegna del piatto
                         Platform.runLater(() -> MainController.getRC().portaPiatto(piatto));
+                        //Aggiungo il piatto alla lista di piatti da lavare
                         Azienda.getAiutoCuoco().piattiDaLavare.add(piatto);
                         Thread.sleep(3500);
+                        //Dopo 3 secondi nascondo il piatto
                         Platform.runLater(() -> MainController.getRC().nascondiPortaPiatto());
                     } catch (InterruptedException e) {
                     }
                 }
                 try{
+                    //Quando ho finito di portare i piatti, aspetto 2 secondi e poi chiudo il servizio
                     Thread.sleep(2000);
                     Sala.terminaServizio();
                     Platform.runLater(() -> MainController.getRC().mostraPaneChiusura());
+                    //Chiamo la funzione per lavare i piatti
                     Azienda.getAiutoCuoco().lavaPiatti();
                 }
                 catch(InterruptedException e){
